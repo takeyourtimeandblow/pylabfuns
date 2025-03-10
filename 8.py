661275c9118e9850b6580aa9d0f4a43c0776edb7
Groovie <ermalyukegor572@gmail.com>
2025-03-10 17:47:57 +0700

    second

--- 8.py
+++ 8.py
@@ -1,19 +1,31 @@
 def tic_tac_toe(field):
+    if "x x x" and "o o o" in field:
+        return "someone is cheating! draw"
     if "x x x" in field:
         return "x win"
     if "o o o" in field:
         return "o win"
-    for i in range(0,field[0], 2):
-        mark = field[0][i]
-        if i == 2:
-            if field[1][2] == mark and field[2][2] == mark:
-                return mark + " win"
-
-
+    nested_field = [i.split(' ') for i in field]
+    for i in range(len(nested_field[0])):
+        match i:
+            case 0:
+                if nested_field[0][0] == nested_field[1][1] == nested_field[2][2]:
+                    return nested_field[0][0] + "win"
+            case 2:
+                if nested_field[0][2] == nested_field[1][1] == nested_field[2][0]:
+                    return nested_field[0][2] + "win"
+    vertical0 = ''.join([i[0] for i in nested_field])
+    vertical1 = ''.join([i[1] for i in nested_field])
+    vertical2 = ''.join([i[2] for i in nested_field])
+    vertical_iter = [vertical0, vertical1, vertical2]
+    if "xxx" in vertical_iter:
+        return "x win"
+    if "ooo" in vertical_iter:
+        return "o win"
 
 data = [
-    "x x 0",
-    "0 x 0",
-    "x 0 0"
+    "x x o",
+    "o x o",
+    "x o o"
 ]
 print(tic_tac_toe(data))
